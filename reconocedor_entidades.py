cdceb05803685e3c3f34d7c4613be7582a5a9205
Emilio Francisco Sánchez Martínez <79818976+Emilio1485@users.noreply.github.com>
2023-11-05 21:41:42 -0600

    Add files via upload
    actualización

--- reconocedor_entidades.py
+++ reconocedor_entidades.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.ensemble import RandomForestClassifier
 
 # Abre el archivo CSV en modo lectura, se le da a la columna 1 el nombre de 'Texto' y la columna 2 el nombre de 'Etiqueta'
 datos = pd.read_csv('ner_dataset.csv', names=['Texto', 'Etiqueta'])
+datos1 = pd.read_csv('palabras_etiquetas.csv')
 
-#Imprime solamente una fila
+#Imprime solammente una fila
 def imprimir_elem_tabla():
-    i = int(input("Dame un número de 0 a 5559:\n"))
+    i = int(input("Dame un número de o a 5559:\n"))
     print("\nTabla:\n")
     print(datos.iloc[i])
 
@@ -15,37 +18,50 @@ def imprimir_datos():
     print("\nTabla:\n")
     print(datos)
 
-#Hace minúsculas todas las palabras y muestra todas las filas del archivo
+#Hace minusculas todas las palabras y muestra todas las filas del archivo
 def lower_eti_all():
-    datos['Texto'] = datos['Texto'].str.lower().str.split()
+    datos['Texto']= datos['Texto'].str.lower().str.split()
     etiquetas = datos["Etiqueta"].tolist()
     palabras_etiquetas = [list(zip(texto, etiqueta.split())) for texto, etiqueta in zip(datos['Texto'], etiquetas)]
     datos["Palabras_Etiquetas"] = palabras_etiquetas
-    print(datos['Palabras_Etiquetas'])
+    # Crear un nuevo DataFrame con las palabras y etiquetas
+    nuevo_datos = pd.DataFrame({'Palabra': [item[0] for sublist in palabras_etiquetas for item in sublist],
+                            'Etiqueta': [item[1] for sublist in palabras_etiquetas for item in sublist]})
 
-#Hace minúsculas las palabras y muestra solamente una fila puesta en el input
+    # Se guarda en CSV
+    nuevo_datos.to_csv('palabras_etiquetas.csv', index=False)
+
+    # Imprimir las primeras filas del nuevo archivo
+    print(nuevo_datos.head())
+
+
+
+#Hace minusculas las palabras y muestra solamente una fila puesta en el input
 def lower_eti_uno():
-    i = int(input("Dame un número de 0 a 5559:\n"))
-    datos['Texto'] = datos['Texto'].str.lower().str.split()
+    i = int(input("Dame un número de o a 5559:\n"))
+    datos['Texto']= datos['Texto'].str.lower().str.split()
     etiquetas = datos["Etiqueta"].tolist()
     palabras_etiquetas = [list(zip(texto, etiqueta.split())) for texto, etiqueta in zip(datos['Texto'], etiquetas)]
     datos["Palabras_Etiquetas"] = palabras_etiquetas
     print(datos['Palabras_Etiquetas'].iloc[i])
 
-# Hace minúsculas las palabras
-datos['Texto'] = datos['Texto'].str.lower()
+#Hace la separacion con train_test_split
+def train_test_l():
+    X = datos['Texto']
+    y = datos['Etiqueta']
+    #Minuculas
+    datos['Texto']= datos['Texto'].str.lower().str.split()
 
-# Separar los datos en características (X) y etiquetas (y)
-X = datos['Texto']
-y = datos['Etiqueta']
+    #Separación con train_test_split y imprime resultados
+    X_train, X_eval, y_train, y_eval = train_test_split(X, y, test_size=0.3, random_state=42)
+    print("Tamaño del conjunto de entrenamiento:", len(X_train))
+    print("Tamaño del conjunto de evaluación:", len(X_eval))
 
-# Separar los datos en un 70% de entrenamiento y un 30% de evaluación
-X_train, X_eval, y_train, y_eval = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Imprimir tamaños de los conjuntos de entrenamiento y evaluación
-print("Tamaño del conjunto de entrenamiento:", len(X_train))
-print("Tamaño del conjunto de evaluación:", len(X_eval))
 
-# Llama a las funciones que proporcionaste
+# Funciones en acción
 # imprimir_datos()
 # lower_eti_uno()
+train_test_l()
+
+
