7bdda784c0321c5385110ec0f431d7a21bcefdf2
TenochFlores <tenoch@MacBook-Air-de-Tenoch.local>
2023-11-05 20:50:23 -0600

    3. train_test_split

--- reconocedor_entidades.py
+++ reconocedor_entidades.py
@@ -1,11 +1,12 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # Abre el archivo CSV en modo lectura, se le da a la columna 1 el nombre de 'Texto' y la columna 2 el nombre de 'Etiqueta'
 datos = pd.read_csv('ner_dataset.csv', names=['Texto', 'Etiqueta'])
 
-#Imprime solammente una fila
+#Imprime solamente una fila
 def imprimir_elem_tabla():
-    i = int(input("Dame un número de o a 5559:\n"))
+    i = int(input("Dame un número de 0 a 5559:\n"))
     print("\nTabla:\n")
     print(datos.iloc[i])
 
@@ -14,24 +15,37 @@ def imprimir_datos():
     print("\nTabla:\n")
     print(datos)
 
-#Hace minusculas todas las palabras y muestra todas las filas del archivo
+#Hace minúsculas todas las palabras y muestra todas las filas del archivo
 def lower_eti_all():
-    datos['Texto']= datos['Texto'].str.lower().str.split()
+    datos['Texto'] = datos['Texto'].str.lower().str.split()
     etiquetas = datos["Etiqueta"].tolist()
     palabras_etiquetas = [list(zip(texto, etiqueta.split())) for texto, etiqueta in zip(datos['Texto'], etiquetas)]
     datos["Palabras_Etiquetas"] = palabras_etiquetas
     print(datos['Palabras_Etiquetas'])
 
-#Hace minusculas las palabras y muestra solamente una fila puesta en el input
+#Hace minúsculas las palabras y muestra solamente una fila puesta en el input
 def lower_eti_uno():
-    i = int(input("Dame un número de o a 5559:\n"))
-    datos['Texto']= datos['Texto'].str.lower().str.split()
+    i = int(input("Dame un número de 0 a 5559:\n"))
+    datos['Texto'] = datos['Texto'].str.lower().str.split()
     etiquetas = datos["Etiqueta"].tolist()
     palabras_etiquetas = [list(zip(texto, etiqueta.split())) for texto, etiqueta in zip(datos['Texto'], etiquetas)]
     datos["Palabras_Etiquetas"] = palabras_etiquetas
     print(datos['Palabras_Etiquetas'].iloc[i])
- 
 
-imprimir_datos()
-lower_eti_uno()
+# Hace minúsculas las palabras
+datos['Texto'] = datos['Texto'].str.lower()
 
+# Separar los datos en características (X) y etiquetas (y)
+X = datos['Texto']
+y = datos['Etiqueta']
+
+# Separar los datos en un 70% de entrenamiento y un 30% de evaluación
+X_train, X_eval, y_train, y_eval = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Imprimir tamaños de los conjuntos de entrenamiento y evaluación
+print("Tamaño del conjunto de entrenamiento:", len(X_train))
+print("Tamaño del conjunto de evaluación:", len(X_eval))
+
+# Llama a las funciones que proporcionaste
+# imprimir_datos()
+# lower_eti_uno()
